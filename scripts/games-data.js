381f84ac3b2c53c8ecce4fb693640088237c8441
NeonWave <96406764+Zonikyo@users.noreply.github.com>
2025-03-12 17:02:52 +1100

    Create games-data.js

---
+++ scripts/games-data.js
@@ -0,0 +1,346 @@
+// scripts/games-data.js
+const gamesData = [
+    {
+        "name": "Eaglercraft 1.5.2 (Eagtek)",
+        "description": "Description for Eaglercraft 1.5.2 (Eagtek)",
+        "image": "neonwave-load.png",
+        "url": "https://eaglercraftgame.github.io/go/minecraft-1.5.2/"
+    },
+    {
+        "name": "Eaglercraft X 1.8 (Astraclient)",
+        "description": "Description for Eaglercraft X 1.8 (Astraclient)",
+        "image": "neonwave-load.png",
+        "url": "https://astraclientorg.github.io/"
+    },
+    {
+        "name": "Minecraft Classic (Mojang Studios)",
+        "description": "Description for Minecraft Classic (Mojang Studios)",
+        "image": "neonwave-load.png",
+        "url": "https://minecraftunblocked.gitlab.io/go/minecraft-beta/"
+    },
+    {
+        "name": "Slope",
+        "description": "Description for Slope",
+        "image": "neonwave-load.png",
+        "url": "https://theslope.github.io/games/slope/index.html"
+    },
+    {
+        "name": "Slope 2",
+        "description": "Description for Slope 2",
+        "image": "neonwave-load.png",
+        "url": "https://slope-2.gitlab.io/file/"
+    },
+    {
+        "name": "Slope Multiplayer",
+        "description": "Description for Slope Multiplayer",
+        "image": "neonwave-load.png",
+        "url": "https://education76.github.io/g7/slope-2-multiplayer/"
+    },
+    {
+        "name": "1v1 lol",
+        "description": "Description for 1v1 lol",
+        "image": "neonwave-load.png",
+        "url": "https://1vl.school/"
+    },
+    {
+        "name": "Drive Mad",
+        "description": "Description for Drive Mad",
+        "image": "neonwave-load.png",
+        "url": "https://drive-madgame.github.io/file/"
+    },
+    {
+        "name": "Run",
+        "description": "Description for Run",
+        "image": "neonwave-load.png",
+        "url": "https://dnrweqffuwjtx.cloudfront.net/games/2024/flash/run-1/index.html"
+    },
+    {
+        "name": "Run 2",
+        "description": "Description for Run 2",
+        "image": "neonwave-load.png",
+        "url": "https://ubg89.github.io/Run2/"
+    },
+    {
+        "name": "Run 3",
+        "description": "Description for Run 3",
+        "image": "neonwave-load.png",
+        "url": "https://lekug.github.io/tn6pS9dCf37xAhkJv/"
+    },
+    {
+        "name": "Flappy Bird",
+        "description": "Description for Flappy Bird",
+        "image": "neonwave-load.png",
+        "url": "https://ccported.github.io/fb/"
+    },
+    {
+        "name": "Moto X3M",
+        "description": "Description for Moto X3M",
+        "image": "neonwave-load.png",
+        "url": "https://motox3m.gitlab.io/game/moto-x3m/"
+    },
+    {
+        "name": "Moto X3M Extended",
+        "description": "Description for Moto X3M Extended",
+        "image": "neonwave-load.png",
+        "url": "https://motox3m.gitlab.io/x3m-1.20.1p/"
+    },
+    {
+        "name": "Moto X3M Pool Party",
+        "description": "Description for Moto X3M Pool Party",
+        "image": "neonwave-load.png",
+        "url": "https://education76.github.io/g/moto-x3m-pool-party/"
+    },
+    {
+        "name": "Moto X3M Winter",
+        "description": "Description for Moto X3M Winter",
+        "image": "neonwave-load.png",
+        "url": "https://motox3munblocked.github.io/4-winter/"
+    },
+    {
+        "name": "Duck Life",
+        "description": "Description for Duck Life",
+        "image": "neonwave-load.png",
+        "url": "https://ducklifegame.github.io/file/"
+    },
+    {
+        "name": "Duck Life 2",
+        "description": "Description for Duck Life 2",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Duck-Life-2/"
+    },
+    {
+        "name": "Duck Life 3",
+        "description": "Description for Duck Life 3",
+        "image": "neonwave-load.png",
+        "url": "https://unblockedgames911.gitlab.io/duck-life-3/"
+    },
+    {
+        "name": "Duck Life 4",
+        "description": "Description for Duck Life 4",
+        "image": "neonwave-load.png",
+        "url": "https://htmlxm.github.io/h/duck-life-4"
+    },
+    {
+        "name": "Stunt Master City",
+        "description": "Description for Stunt Master City",
+        "image": "neonwave-load.png",
+        "url": "https://ubg77.github.io/edit/city-car-driving-stunt-master/"
+    },
+    {
+        "name": "Sky Car Stunt 3D",
+        "description": "Description for Sky Car Stunt 3D",
+        "image": "neonwave-load.png",
+        "url": "https://ubg77.github.io/edit/city-car-driving-stunt-master/"
+    },
+    {
+        "name": "Time Shooter 2",
+        "description": "Description for Time Shooter 2",
+        "image": "neonwave-load.png",
+        "url": "https://gnhustgames.github.io/timeshooter2/"
+    },
+    {
+        "name": "2048",
+        "description": "Description for 2048",
+        "image": "neonwave-load.png",
+        "url": "https://ovolve.github.io/2048-AI/"
+    },
+    {
+        "name": "Sniper Assassin",
+        "description": "Description for Sniper Assassin",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin/"
+    },
+    {
+        "name": "Sniper Assassin 2",
+        "description": "Description for Sniper Assassin 2",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-2/"
+    },
+    {
+        "name": "Sniper Assassin 3",
+        "description": "Description for Sniper Assassin 3",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-3/"
+    },
+    {
+        "name": "Sniper Assassin 4",
+        "description": "Description for Sniper Assassin 4",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-4/"
+    },
+    {
+        "name": "Sniper Assassin 5",
+        "description": "Description for Sniper Assassin 5",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-5/"
+    },
+    {
+        "name": "Tactical Assassin",
+        "description": "Description for Tactical Assassin",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin/"
+    },
+    {
+        "name": "Tactical Assassin 2",
+        "description": "Description for Tactical Assassin 2",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin-2/"
+    },
+    {
+        "name": "Tactical Assassin 3",
+        "description": "Description for Tactical Assassin 3",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin-3/"
+    },
+    {
+        "name": "Five Nights at Freddy’s",
+        "description": "Description for Five Nights at Freddy’s",
+        "image": "neonwave-load.png",
+        "url": "https://fivenightsatfreddysonline.github.io/file/"
+    },
+    {
+        "name": "Backrooms",
+        "description": "Description for Backrooms",
+        "image": "neonwave-load.png",
+        "url": "https://unblockeds-games.github.io/iframe/backrooms/"
+    },
+    {
+        "name": "Quake",
+        "description": "Description for Quake",
+        "image": "neonwave-load.png",
+        "url": "https://gnhustgames.github.io/quake/wwwwsadwsw"
+    },
+    {
+        "name": "Townscaper",
+        "description": "Description for Townscaper",
+        "image": "neonwave-load.png",
+        "url": "https://manscod.github.io/other/townscaper/"
+    },
+    {
+        "name": "Parking Fury",
+        "description": "Description for Parking Fury",
+        "image": "neonwave-load.png",
+        "url": "https://parking-fury.gitlab.io/file/"
+    },
+    {
+        "name": "Parking Fury 2",
+        "description": "Description for Parking Fury 2",
+        "image": "neonwave-load.png",
+        "url": "https://gnhustgames.github.io/parking-fury-2/"
+    },
+    {
+        "name": "Parking Fury 3",
+        "description": "Description for Parking Fury 3",
+        "image": "neonwave-load.png",
+        "url": "https://parkingfury3.github.io/gamefle/parking-fury-3/"
+    },
+    {
+        "name": "Geometry Dash",
+        "description": "Description for Geometry Dash",
+        "image": "neonwave-load.png",
+        "url": "https://geometry-dash.gitlab.io/file/"
+    },
+    {
+        "name": "BulletForce",
+        "description": "Description for BulletForce",
+        "image": "neonwave-load.png",
+        "url": "https://ubg44.github.io/BulletForce/"
+    },
+    {
+        "name": "Cookie Clicker",
+        "description": "Description for Cookie Clicker",
+        "image": "neonwave-load.png",
+        "url": "https://ozh.github.io/cookieclicker/"
+    },
+    {
+        "name": "Fallboys",
+        "description": "Description for Fallboys",
+        "image": "neonwave-load.png",
+        "url": "https://gnhustgames.github.io/fallboys/"
+    },
+    {
+        "name": "Little Alchemy 2",
+        "description": "Description for Little Alchemy 2",
+        "image": "neonwave-load.png",
+        "url": "https://classroom15x.com/coolmath/LittleAlchemy2/?gd_sdk_referrer_url=https://classroom15x.com/play/little-alchemy-2"
+    },
+    {
+        "name": "Smash Karts",
+        "description": "Description for Smash Karts",
+        "image": "neonwave-load.png",
+        "url": "https://smashkartsunblocked.github.io/"
+    },
+    {
+        "name": "Subway Surfers",
+        "description": "Description for Subway Surfers",
+        "image": "neonwave-load.png",
+        "url": "https://subway-surfers.gitlab.io/file/"
+    },
+    {
+        "name": "Monkey Mart",
+        "description": "Description for Monkey Mart",
+        "image": "neonwave-load.png",
+        "url": "https://monkeymartonline.github.io/file/"
+    },
+    {
+        "name": "Eggy Car",
+        "description": "Description for Eggy Car",
+        "image": "neonwave-load.png",
+        "url": "https://webglmath.github.io/eggy-car/"
+    },
+    {
+        "name": "Crossy Road",
+        "description": "Description for Crossy Road",
+        "image": "neonwave-load.png",
+        "url": "https://webglmath.github.io/eggy-car/"
+    },
+    {
+        "name": "8 ball pool",
+        "description": "Description for 8 ball pool",
+        "image": "neonwave-load.png",
+        "url": "https://8-ball-pool-online.github.io/file/"
+    },
+    {
+        "name": "Minesweeper",
+        "description": "Description for Minesweeper",
+        "image": "neonwave-load.png",
+        "url": "https://minesweeper.github.io/"
+    },
+    {
+        "name": "Drift boss",
+        "description": "Description for Drift boss",
+        "image": "neonwave-load.png",
+        "url": "https://drift-boss.gitlab.io/file/"
+    },
+    {
+        "name": "Solitare",
+        "description": "Description for Solitare",
+        "image": "neonwave-load.png",
+        "url": "https://www.google.com/logos/fnbx/solitaire/standalone.html"
+    },
+    {
+        "name": "Tunnel Rush",
+        "description": "Description for Tunnel Rush",
+        "image": "neonwave-load.png",
+        "url": "https://liminthoi.github.io/games/tunnel-rush/index.html"
+    },
+    {
+        "name": "Mad Gunz",
+        "description": "Description for Mad Gunz",
+        "image": "neonwave-load.png",
+        "url": "https://ubgwtf.gitlab.io/mad-gunz/"
+    },
+    {
+        "name": "Happy Wheels",
+        "description": "Description for Happy Wheels",
+        "image": "neonwave-load.png",
+        "url": "https://sreekar617.github.io/hw/index.html"
+    }
+];
+
+// Export the data for use in other scripts
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = gamesData; // For Node.js compatibility (optional)
+} else {
+    window.gamesData = gamesData; // For browser global access
+}
