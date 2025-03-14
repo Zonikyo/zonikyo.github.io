e698c73170c485451ab26a8062422e11161ca894
NeonWave <96406764+Zonikyo@users.noreply.github.com>
2025-03-14 12:02:38 +1100

    Update games-data.js

--- scripts/games-data.js
+++ scripts/games-data.js
@@ -2,337 +2,337 @@
 const gamesData = [
     {
         "name": "Eaglercraft 1.5.2 (Eagtek)",
-        "description": "Description for Eaglercraft 1.5.2 (Eagtek)",
+        "description": "A version of Minecraft 1.5.2 tailored for browser play, offering the classic survival and creative modes.",
         "image": "neonwave-load.png",
         "url": "https://eaglercraftgame.github.io/go/minecraft-1.5.2/"
     },
     {
         "name": "Eaglercraft X 1.8 (Astraclient)",
-        "description": "Description for Eaglercraft X 1.8 (Astraclient)",
+        "description": "An enhanced version of Minecraft 1.8 for browsers, featuring additional client-side improvements and tweaks.",
         "image": "neonwave-load.png",
         "url": "https://astraclientorg.github.io/"
     },
     {
         "name": "Minecraft Classic (Mojang Studios)",
-        "description": "Description for Minecraft Classic (Mojang Studios)",
+        "description": "The original Minecraft experience, playable in a web browser, offering the iconic sandbox gameplay.",
         "image": "neonwave-load.png",
         "url": "https://minecraftunblocked.gitlab.io/go/minecraft-beta/"
     },
     {
         "name": "Slope",
-        "description": "Description for Slope",
+        "description": "A fast-paced 3D running game where you navigate a ball down a steep, endless slope.",
         "image": "neonwave-load.png",
         "url": "https://theslope.github.io/games/slope/index.html"
     },
     {
         "name": "Slope 2",
-        "description": "Description for Slope 2",
+        "description": "The sequel to Slope, featuring more challenging levels and enhanced graphics.",
         "image": "neonwave-load.png",
         "url": "https://slope-2.gitlab.io/file/"
     },
     {
         "name": "Slope Multiplayer",
-        "description": "Description for Slope Multiplayer",
+        "description": "A multiplayer version of Slope where you can compete against other players.",
         "image": "neonwave-load.png",
         "url": "https://education76.github.io/g7/slope-2-multiplayer/"
     },
     {
         "name": "1v1 lol",
-        "description": "Description for 1v1 lol",
+        "description": "A competitive building and shooting game where players can challenge each other in 1v1 battles.",
         "image": "neonwave-load.png",
         "url": "https://1vl.school/"
     },
     {
         "name": "Drive Mad",
-        "description": "Description for Drive Mad",
+        "description": "A driving game focused on performing stunts and overcoming various obstacles.",
         "image": "neonwave-load.png",
         "url": "https://drive-madgame.github.io/file/"
     },
     {
         "name": "Run",
-        "description": "Description for Run",
+        "description": "A classic platformer where you run and jump through a series of challenging levels.",
         "image": "neonwave-load.png",
         "url": "https://dnrweqffuwjtx.cloudfront.net/games/2024/flash/run-1/index.html"
     },
     {
         "name": "Run 2",
-        "description": "Description for Run 2",
+        "description": "The sequel to Run with more complex levels and new characters.",
         "image": "neonwave-load.png",
         "url": "https://ubg89.github.io/Run2/"
     },
     {
         "name": "Run 3",
-        "description": "Description for Run 3",
+        "description": "The third installment in the Run series, featuring new levels and gameplay mechanics.",
         "image": "neonwave-load.png",
         "url": "https://lekug.github.io/tn6pS9dCf37xAhkJv/"
     },
     {
         "name": "Flappy Bird",
-        "description": "Description for Flappy Bird",
+        "description": "A simple yet addictive arcade game where you navigate a bird through pipes.",
         "image": "neonwave-load.png",
         "url": "https://ccported.github.io/fb/"
     },
     {
         "name": "Moto X3M",
-        "description": "Description for Moto X3M",
+        "description": "A motocross racing game with challenging tracks and obstacles.",
         "image": "neonwave-load.png",
         "url": "https://motox3m.gitlab.io/game/moto-x3m/"
     },
     {
         "name": "Moto X3M Extended",
-        "description": "Description for Moto X3M Extended",
+        "description": "An extended version of Moto X3M with additional levels.",
         "image": "neonwave-load.png",
         "url": "https://motox3m.gitlab.io/x3m-1.20.1p/"
     },
     {
         "name": "Moto X3M Pool Party",
-        "description": "Description for Moto X3M Pool Party",
+        "description": "A summer-themed version of Moto X3M.",
         "image": "neonwave-load.png",
         "url": "https://education76.github.io/g/moto-x3m-pool-party/"
     },
     {
         "name": "Moto X3M Winter",
-        "description": "Description for Moto X3M Winter",
+        "description": "A winter-themed version of Moto X3M.",
         "image": "neonwave-load.png",
         "url": "https://motox3munblocked.github.io/4-winter/"
     },
     {
         "name": "Duck Life",
-        "description": "Description for Duck Life",
+        "description": "A game where you raise and train a duck to compete in races.",
         "image": "neonwave-load.png",
         "url": "https://ducklifegame.github.io/file/"
     },
     {
         "name": "Duck Life 2",
-        "description": "Description for Duck Life 2",
+        "description": "The sequel to Duck Life with more features and challenges.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Duck-Life-2/"
     },
     {
         "name": "Duck Life 3",
-        "description": "Description for Duck Life 3",
+        "description": "Another sequel in the Duck Life series with enhanced gameplay.",
         "image": "neonwave-load.png",
         "url": "https://unblockedgames911.gitlab.io/duck-life-3/"
     },
     {
         "name": "Duck Life 4",
-        "description": "Description for Duck Life 4",
+        "description": "The fourth installment in the Duck Life series.",
         "image": "neonwave-load.png",
         "url": "https://htmlxm.github.io/h/duck-life-4"
     },
     {
         "name": "Stunt Master City",
-        "description": "Description for Stunt Master City",
+        "description": "A stunt driving game set in a city environment.",
         "image": "neonwave-load.png",
         "url": "https://ubg77.github.io/edit/city-car-driving-stunt-master/"
     },
     {
         "name": "Sky Car Stunt 3D",
-        "description": "Description for Sky Car Stunt 3D",
+        "description": "A 3D car stunt game with challenging tracks.",
         "image": "neonwave-load.png",
         "url": "https://ubg77.github.io/edit/city-car-driving-stunt-master/"
     },
     {
         "name": "Time Shooter 2",
-        "description": "Description for Time Shooter 2",
+        "description": "A shooting game where time moves only when you move.",
         "image": "neonwave-load.png",
         "url": "https://gnhustgames.github.io/timeshooter2/"
     },
     {
         "name": "2048",
-        "description": "Description for 2048",
+        "description": "A sliding puzzle game where you combine tiles to reach the number 2048.",
         "image": "neonwave-load.png",
         "url": "https://ovolve.github.io/2048-AI/"
     },
     {
         "name": "Sniper Assassin",
-        "description": "Description for Sniper Assassin",
+        "description": "A shooting game where you play as a sniper completing various missions.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin/"
     },
     {
         "name": "Sniper Assassin 2",
-        "description": "Description for Sniper Assassin 2",
+        "description": "The sequel to Sniper Assassin with new missions.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-2/"
     },
     {
         "name": "Sniper Assassin 3",
-        "description": "Description for Sniper Assassin 3",
+        "description": "The third installment in the Sniper Assassin series.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-3/"
     },
     {
         "name": "Sniper Assassin 4",
-        "description": "Description for Sniper Assassin 4",
+        "description": "Another sequel in the Sniper Assassin series.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-4/"
     },
     {
         "name": "Sniper Assassin 5",
-        "description": "Description for Sniper Assassin 5",
+        "description": "The fifth installment in the Sniper Assassin series.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Sniper-Assassin-5/"
     },
     {
         "name": "Tactical Assassin",
-        "description": "Description for Tactical Assassin",
+        "description": "A tactical shooting game with various missions.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin/"
     },
     {
         "name": "Tactical Assassin 2",
-        "description": "Description for Tactical Assassin 2",
+        "description": "The sequel to Tactical Assassin with new missions.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin-2/"
     },
     {
         "name": "Tactical Assassin 3",
-        "description": "Description for Tactical Assassin 3",
+        "description": "Another sequel in the Tactical Assassin series.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/Tactical-Assassin-3/"
     },
     {
         "name": "Five Nights at Freddy’s",
-        "description": "Description for Five Nights at Freddy’s",
+        "description": "A horror game where you survive nights in a haunted pizzeria.",
         "image": "neonwave-load.png",
         "url": "https://fivenightsatfreddysonline.github.io/file/"
     },
     {
         "name": "Backrooms",
-        "description": "Description for Backrooms",
+        "description": "An exploration game set in the eerie and endless backrooms.",
         "image": "neonwave-load.png",
         "url": "https://unblockeds-games.github.io/iframe/backrooms/"
     },
     {
         "name": "Quake",
-        "description": "Description for Quake",
+        "description": "A classic first-person shooter game.",
         "image": "neonwave-load.png",
         "url": "https://gnhustgames.github.io/quake/wwwwsadwsw"
     },
     {
         "name": "Townscaper",
-        "description": "Description for Townscaper",
+        "description": "A city-building game with a focus on creativity and aesthetics.",
         "image": "neonwave-load.png",
         "url": "https://manscod.github.io/other/townscaper/"
     },
     {
         "name": "Parking Fury",
-        "description": "Description for Parking Fury",
+        "description": "A parking simulation game with various challenges.",
         "image": "neonwave-load.png",
         "url": "https://parking-fury.gitlab.io/file/"
     },
     {
         "name": "Parking Fury 2",
-        "description": "Description for Parking Fury 2",
+        "description": "The sequel to Parking Fury with new levels.",
         "image": "neonwave-load.png",
         "url": "https://gnhustgames.github.io/parking-fury-2/"
     },
     {
         "name": "Parking Fury 3",
-        "description": "Description for Parking Fury 3",
+        "description": "Another sequel in the Parking Fury series.",
         "image": "neonwave-load.png",
         "url": "https://parkingfury3.github.io/gamefle/parking-fury-3/"
     },
     {
         "name": "Geometry Dash",
-        "description": "Description for Geometry Dash",
+        "description": "A rhythm-based platformer with challenging levels.",
         "image": "neonwave-load.png",
         "url": "https://geometry-dash.gitlab.io/file/"
     },
     {
         "name": "BulletForce",
-        "description": "Description for BulletForce",
+        "description": "A first-person shooter game with various multiplayer modes.",
         "image": "neonwave-load.png",
         "url": "https://ubg44.github.io/BulletForce/"
     },
     {
         "name": "Cookie Clicker",
-        "description": "Description for Cookie Clicker",
+        "description": "An incremental game where you click to produce cookies.",
         "image": "neonwave-load.png",
         "url": "https://ozh.github.io/cookieclicker/"
     },
     {
         "name": "Fallboys",
-        "description": "Description for Fallboys",
+        "description": "A multiplayer party game with various mini-games.",
         "image": "neonwave-load.png",
         "url": "https://gnhustgames.github.io/fallboys/"
     },
     {
         "name": "Little Alchemy 2",
-        "description": "Description for Little Alchemy 2",
+        "description": "A puzzle game where you combine elements to create new items.",
         "image": "neonwave-load.png",
         "url": "https://classroom15x.com/coolmath/LittleAlchemy2/?gd_sdk_referrer_url=https://classroom15x.com/play/little-alchemy-2"
     },
     {
         "name": "Smash Karts",
-        "description": "Description for Smash Karts",
+        "description": "A multiplayer kart racing game with weapons.",
         "image": "neonwave-load.png",
         "url": "https://smashkartsunblocked.github.io/"
     },
     {
         "name": "Subway Surfers",
-        "description": "Description for Subway Surfers",
+        "description": "An endless runner game where you dodge obstacles and collect coins.",
         "image": "neonwave-load.png",
         "url": "https://subway-surfers.gitlab.io/file/"
     },
     {
         "name": "Monkey Mart",
-        "description": "Description for Monkey Mart",
+        "description": "A management game where you run a monkey-themed grocery store.",
         "image": "neonwave-load.png",
         "url": "https://monkeymartonline.github.io/file/"
     },
     {
         "name": "Eggy Car",
-        "description": "Description for Eggy Car",
+        "description": "A driving game where you balance an egg on a car.",
         "image": "neonwave-load.png",
         "url": "https://webglmath.github.io/eggy-car/"
     },
     {
         "name": "Crossy Road",
-        "description": "Description for Crossy Road",
+        "description": "An arcade game where you help a character cross roads and avoid obstacles.",
         "image": "neonwave-load.png",
         "url": "https://webglmath.github.io/eggy-car/"
     },
     {
         "name": "8 ball pool",
-        "description": "Description for 8 ball pool",
+        "description": "A classic pool game playable in a browser.",
         "image": "neonwave-load.png",
         "url": "https://8-ball-pool-online.github.io/file/"
     },
     {
         "name": "Minesweeper",
-        "description": "Description for Minesweeper",
+        "description": "The classic puzzle game where you clear a minefield.",
         "image": "neonwave-load.png",
         "url": "https://minesweeper.github.io/"
     },
     {
         "name": "Drift boss",
-        "description": "Description for Drift boss",
+        "description": "A drifting game where you navigate through various tracks.",
         "image": "neonwave-load.png",
         "url": "https://drift-boss.gitlab.io/file/"
     },
     {
         "name": "Solitare",
-        "description": "Description for Solitare",
+        "description": "The classic card game, Solitaire.",
         "image": "neonwave-load.png",
         "url": "https://www.google.com/logos/fnbx/solitaire/standalone.html"
     },
     {
         "name": "Tunnel Rush",
-        "description": "Description for Tunnel Rush",
+        "description": "A fast-paced tunnel racing game.",
         "image": "neonwave-load.png",
         "url": "https://liminthoi.github.io/games/tunnel-rush/index.html"
     },
     {
         "name": "Mad Gunz",
-        "description": "Description for Mad Gunz",
+        "description": "A multiplayer first-person shooter game.",
         "image": "neonwave-load.png",
         "url": "https://ubgwtf.gitlab.io/mad-gunz/"
     },
     {
         "name": "Happy Wheels",
-        "description": "Description for Happy Wheels",
+        "description": "A ragdoll physics-based platformer with various levels.",
         "image": "neonwave-load.png",
         "url": "https://sreekar617.github.io/hw/index.html"
     }
